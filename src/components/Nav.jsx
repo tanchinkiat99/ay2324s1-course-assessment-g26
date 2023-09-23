2ee30fda7e90ee6982f89b05827d2b8cd705beef
Felix <felixongweicong@gmail.com>
2023-09-23 10:26:57 +0800

    Add logo to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,8 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
+        <Image className="object-contain" src="/logo.svg" width={50} height={50}/>
+        <p className="max-sm:hidden font-satoshi font-semibold text-lg text-black tracking-wide">PeerPrep</p>
       </Link>
     </nav>
   );

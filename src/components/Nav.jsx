5cccca33dbc75f97e394fe9ce08473f3f0ae1f8e
Felix <felixongweicong@gmail.com>
2023-09-25 15:59:21 +0800

    Add test api endpoint

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
@@ -12,40 +12,58 @@ const Nav = () => {
     const setupProviders = async () => {
       const response = await getProviders();
       setProviders(response);
-    }
+    };
     setupProviders();
   }, []);
 
   return (
     <nav className="flex justify-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-5 flex-center items-center">
-        <Image className="object-contain" src="/logo.svg" width={80} height={80}/>
+        <Image
+          className="object-contain"
+          src="/logo.svg"
+          width={80}
+          height={80}
+        />
         <p className="logo_txt">PeerPrep</p>
       </Link>
       {/* Show signin and signout */}
       <div className="sm:flex hidden">
         {session?.user ? (
           <div className="flex gap-3">
-            <Link href="/questions" className="black_btn">Create Question</Link>
-            <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
+            <Link href="/add-question" className="black_btn">
+              Create Question
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
             <Link href="/profile">
-              <Image src={session?.user.image} width={50} height={50} className="rounded-full"/>
+              <Image
+                src={session?.user.image}
+                width={50}
+                height={50}
+                className="rounded-full"
+              />
             </Link>
           </div>
         ) : (
           <>
-            {providers && 
+            {providers &&
               Object.values(providers).map((provider) => (
-                <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="outline_btn">
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="outline_btn"
+                >
                   Sign In
                 </button>
               ))}
           </>
-        )
-        }
+        )}
       </div>
     </nav>
   );
-}
+};
 
 export default Nav;

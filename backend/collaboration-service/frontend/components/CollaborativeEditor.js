c8a4a07c7d52e42d058e541402dc03ff07fd6eff
itsleeqian <itsleeqian@gmail.com>
2023-10-03 15:08:23 +0800

    integrated ace editor

--- backend/collaboration-service/frontend/components/CollaborativeEditor.js
+++ backend/collaboration-service/frontend/components/CollaborativeEditor.js
@@ -1,3 +1,6 @@
+import ReactAce from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
 import { useEffect, useRef } from 'react';
 import { initYjs, getYText } from '../utils/yjsSetup';
 
@@ -7,24 +10,39 @@ export default function CollaborativeEditor() {
   useEffect(() => {
     initYjs('demo-room');
     const ytext = getYText();
+    let isSyncing = false; // Flag to check if syncing is ongoing
 
-    // Listen to updates from ytext and update textarea
-    const updateHandler = () => {
-        editorRef.current.value = ytext.toString();
-      };
-      ytext.observe(updateHandler);
-      updateHandler();  // initialize
-  
-      // Listen to updates from textarea and update ytext
-      editorRef.current.addEventListener('input', () => {
-        const inputValue = editorRef.current.value;
-        ytext.delete(0, ytext.length); // Clear existing text
-        ytext.insert(0, inputValue);  // Insert new text       
-      });
-    
+    // Initialize Ace Editor from Yjs
+    const editor = editorRef.current.editor;
+    editor.setValue(ytext.toString());
+
+    // Update Ace editor when ytext changes
+    ytext.observe(() => {
+      if (isSyncing) return; // Skip if we are currently syncing
+      isSyncing = true; // Set flag to true
+      editor.setValue(ytext.toString());
+      isSyncing = false; // Reset flag
+    });
+
+    // Observe Ace Editor changes and update Yjs
+    editor.getSession().on('change', () => {
+      if (isSyncing) return; // Skip if we are currently syncing
+      isSyncing = true; // Set flag to true
+      const inputValue = editor.getValue();
+      ytext.delete(0, ytext.length); // Clear existing text
+      ytext.insert(0, inputValue);  // Insert new text
+      isSyncing = false; // Reset flag
+    });
+   
   }, []);
 
   return (
-    <textarea ref={editorRef}></textarea> // Placeholder; replace this with your code editor component
+    <ReactAce
+      mode="javascript"
+      theme="monokai"
+      ref={editorRef}
+      // your other ace editor options here
+    />
+    //<textarea ref={editorRef}></textarea> // Placeholder; replace this with your code editor component
   );
 }

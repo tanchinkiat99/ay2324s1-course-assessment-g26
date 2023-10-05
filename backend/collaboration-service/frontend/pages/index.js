cd092b9fb8e10d17db9800b5e32a848388a30438
itsleeqian <itsleeqian@gmail.com>
2023-10-05 23:33:14 +0800

    Added basic frontend

--- backend/collaboration-service/frontend/pages/index.js
+++ backend/collaboration-service/frontend/pages/index.js
@@ -1,9 +1,17 @@
-import CollaborativeEditor from '../components/CollaborativeEditor';
+import CollaborativeEditor from '../components/CodeEditor';
 export default function Home() {
   return (
-    <div>
-      <h1>PeerPrep Collaborative Editor</h1>
-      <CollaborativeEditor />
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ flex: 1, padding: '20px', borderRight: '1px solid black' }}>
+        <h1>Title: Sample Problem</h1>
+        <p>Description: Here's a sample problem description. Solve XYZ using ABC method.</p>
+        <p>Categories: Algorithms, Arrays</p>
+        <p>Complexity: Medium</p>
+      </div>
+      <div style={{ flex: 2, padding: '20px' }}>
+        <h2>Editor</h2>
+        <CollaborativeEditor />
+      </div>
     </div>
   );
 }

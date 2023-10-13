d1d355ac4077587461a6f679834f89ae60caecb0
csiongn <choonsiongn@gmail.com>
2023-10-13 17:40:38 +0800

    Clean up database files

--- user-service/backend/db/migrations/01. schema.sql
+++ user-service/backend/db/migrations/01. schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE clientuser;
 
-CREATE TABLE clientuser (
+CREATE TABLE IF NOT EXISTS clientuser (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,

635c659a300a8265ff8ec10339270d0b711a570f
csiongn <choonsiongn@gmail.com>
2023-10-24 01:15:46 +0800

    Update user schema

--- backend/user-service/db/migrations/01.schema.sql
+++ backend/user-service/db/migrations/01.schema.sql
@@ -1,61 +1,7 @@
-DROP TABLE clientuser;
-
-CREATE TABLE IF NOT EXISTS clientuser (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS clientuser
+(
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR(255)        NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255),
-    google_id VARCHAR(255),
-    auth_method VARCHAR(10) CHECK (auth_method IN ('local', 'google')),
-    role_type VARCHAR(20) CHECK (role_type IN ('user', 'admin')) DEFAULT 'user',
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    modified_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
-
-CREATE TABLE IF NOT EXISTS verification_token
-(
-    identifier TEXT NOT NULL,
-    expires TIMESTAMPTZ NOT NULL,
-    token TEXT NOT NULL,
-
-    PRIMARY KEY (identifier, token)
-);
-
-CREATE TABLE IF NOT EXISTS accounts
-(
-    id SERIAL,
-    "userId" INTEGER NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    provider VARCHAR(255) NOT NULL,
-    "providerAccountId" VARCHAR(255) NOT NULL,
-    refresh_token TEXT,
-    access_token TEXT,
-    expires_at BIGINT,
-    id_token TEXT,
-    scope TEXT,
-    session_state TEXT,
-    token_type TEXT,
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS sessions
-(
-    id SERIAL,
-    "userId" INTEGER NOT NULL,
-    expires TIMESTAMPTZ NOT NULL,
-    "sessionToken" VARCHAR(255) NOT NULL,
-
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS users
-(
-    id SERIAL,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    "emailVerified" TIMESTAMPTZ,
-    image TEXT,
-
-    PRIMARY KEY (id)
-);
+    image VARCHAR(255)        NOT NULL,
+)

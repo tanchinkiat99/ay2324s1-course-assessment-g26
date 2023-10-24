71b1c5f6efb6b01270e2cf0e17c74f8975a7f63b
Choon Siong <37247001+csiongn@users.noreply.github.com>
2023-10-24 12:04:06 +0800

    Remove unused user database functions

--- backend/user-service/database.js
+++ backend/user-service/database.js
@@ -57,26 +57,11 @@ export async function getUserCompleteByEmail(email) {
     return rows[0];
 }
 
-// Fetch password hashed for verification
-export async function getUserPasswordByEmail(email) {
-    const { rows } = await pool.query("SELECT password_hash FROM clientuser WHERE email = $1", [email]);
-    if (rows.length) {
-        return rows[0].password_hash;
-    }
-    return null;
-}
-
 export async function insertUser(email, name, image, user_role) {
     let result;
 
     result = await pool.query("INSERT INTO clientuser (email, name, image, user_role) VALUES ($1, $2, $3, $4)", [email, name, image, user_role])
 
-    return result.rows[0];
-}
-
-export async function updateUserPassword(email, password) {
-    const hashedPassword = await hashPassword(password);
-    const result = await pool.query("UPDATE clientuser SET password_hash = $1 WHERE email = $2", [hashedPassword, email]);
     return result.rows[0];
 }
 

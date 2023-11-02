a6335a43781efa0788aa6fbe591493f32640f50e
csiongn <choonsiongn@gmail.com>
2023-11-02 19:12:30 +0800

    Modify attempt api to retrieve email from request parameters

--- backend/user-service/routes/attempt.js
+++ backend/user-service/routes/attempt.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { addAttempt, getAttemptsByUser } from "../db/controllers/attemptController.js"
-import { body, validationResult } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
 import { checkUserExists } from "../middlewares/userMiddleware.js";
 import { checkQuestionExists } from "../middlewares/attemptMiddleware.js";
 
@@ -10,30 +10,31 @@ dotenv.config();
 const router = express.Router();
 
 // Create new question attempt by a user (A user is uniquely identified by their email)
-router.post('/:email', body(['email', 'question_id', 'question_title', 'code']).notEmpty().escape(),
+router.post('/:email', param('email').isEmail().escape(), body(['question_id', 'question_title', 'code']).notEmpty().escape(),
     checkUserExists(), checkQuestionExists(), async (req, res) => {
-    try {
-        const validationRes = validationResult(req);
-        if (!(validationRes.isEmpty())) { // If validation fails
-            res.status(400).json(validationRes.array()); // Return all error messages
+        try {
+            const validationRes = validationResult(req);
+            if (!(validationRes.isEmpty())) { // If validation fails
+                res.status(400).json(validationRes.array()); // Return all error messages
+            }
+            const { email } = req.params;
+            const { question_id, question_title, code} = req.body;
+            const result = await addAttempt(email, question_id, question_title, code);
+            res.status(200).json({message: `Attempt for question ${question_id} by ${email} created successfully`})
+        } catch (error) {
+            console.log(error);
+            res.status(502).json({message: 'Internal Server Error'});
         }
-        const { email, question_id, question_title, code} = req.body;
-        const result = await addAttempt(email, question_id, question_title, code);
-        res.status(200).json({message: `Attempt for question ${question_id} by ${email} created successfully`})
-    } catch (error) {
-        console.log(error);
-        res.status(502).json({message: 'Internal Server Error'});
-    }
-});
+    });
 
 // Get all attempts by a user
-router.get('/:email', body('email').notEmpty().isEmail().escape(), checkUserExists(), async (req, res) => {
+router.get('/:email', param('email').notEmpty().isEmail().escape(), checkUserExists(), async (req, res) => {
     try {
         const validationRes = validationResult(req);
         if (!validationRes.isEmpty()) { // If validation fails
             res.status(400).json(validationRes.array()); // Return all error messages
         }
-        const { email } = req.body;
+        const { email } = req.params;
         const result = await getAttemptsByUser(email);
         res.status(200).json(result);
     } catch (error) {

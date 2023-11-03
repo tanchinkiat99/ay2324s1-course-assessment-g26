11a57ff42672e7ceaa47492a3a5c69eba101a5b4
csiongn <choonsiongn@gmail.com>
2023-11-03 18:21:50 +0800

    Add schema for user-service tables

---
+++ backend/user-service/db/migrations/01.schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS clientuser (
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR(50)        NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    image VARCHAR(255)       NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS attempt (
+    email VARCHAR(50),
+    question_id VARCHAR(30),
+    question_title VARCHAR(30),
+    attempt_datetime TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    code VARCHAR(300),
+    PRIMARY KEY(email, question_id, attempt_datetime)
+);

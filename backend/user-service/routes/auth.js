31424b43f7f861d16e05c34f373cac40b3842185
csiongn <choonsiongn@gmail.com>
2023-11-10 17:44:39 +0800

    Remove unused code

--- backend/user-service/routes/auth.js
+++ backend/user-service/routes/auth.js
@@ -1,47 +1,13 @@
 //backend/user-service/routes/auth.js
-import cookie from 'cookie';
 import dotenv from 'dotenv';
 import express from 'express';
 // Middleware
 import { body, validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
-
+// Controllers
 import { getUserCompleteByEmail, insertUser } from '../db/controllers/userController.js';
-import { OAuth2Client } from 'google-auth-library';
 
 dotenv.config();
 const router = express.Router();
-const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
-// Create a JWT token
-function createToken(payload) {
-    return jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '1h' });
-}
-
-const verifyTokenFromCookie = (req, res, next) => {
-    const token = req.cookies.auth;
-    if (!token) return res.status(401).send('Access Denied');
-
-    try {
-        const verified = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = verified;
-        next();
-    } catch (err) {
-        res.status(400).send('Invalid Token');
-    }
-};
-
-router.post('/signout', (req, res) => {
-    res.setHeader('Set-Cookie', cookie.serialize('auth', '', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: -1,
-        path: '/'
-    }));
-
-    res.status(200).json({ message: 'Logged out' });
-});
 
 router.post('/signin-new', body(['name', 'email']).notEmpty().escape(), body('image').notEmpty(), body('email').isEmail(),
     async (req, res) => {
@@ -57,7 +23,7 @@ router.post('/signin-new', body(['name', 'email']).notEmpty().escape(), body('im
         let user = await getUserCompleteByEmail(email);
         if (!user) {
             const USER_ROLE = 'user'; // Set default user type created as user
-            user = await insertUser(email, name, image, USER_ROLE);
+            await insertUser(email, name, image, USER_ROLE);
 
             return res.status(200).send({message: `New user ${email} successfully logged in by google`, name: name,
                 email: email, image: image, role_type: USER_ROLE});
